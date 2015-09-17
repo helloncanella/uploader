ac129e92370926530c3c225a05c4fe8beaa8a482
@helloncanella <helloncanella@gmail.com>
2015-09-17 14:01:55 -0300

    substitution of files ability is added

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,5 +1,5 @@
 var acceptedSubtitles = ["text/vtt", "application/x-subrip"];
-var supportedVideos = ["video/mp4", "video/mp4"];
+var supportedVideos = ["video/mp4", "video/ogg", "video/webm"];
 
 var videoFile, subtitleFile, subtitleURL;
 
@@ -13,66 +13,64 @@ $("#dropRegion").on({
     event.preventDefault();
   },
   drop: function(event) {
-
     event.stopPropagation();
     event.preventDefault();
 
     var file = event.originalEvent.dataTransfer.files[0];
     var format = file.type;
 
-    var isVideo = new RegExp("video\/*").test(format);
+    var isSupportedVideo = !!~supportedVideos.indexOf(format);
     var isSubtitle = !!~acceptedSubtitles.indexOf(format);
 
-    if (isVideo) {
-      var isASupportedFormart = !!~supportedVideos.indexOf(format)
-      video = document.createElement('video');
-      videoFile = file;
+    if (isSupportedVideo) {
 
-      if (isASupportedFormart) {
+      if (!videoFile) {
+        video = document.createElement('video');
+        videoFile = file;
         if (!subtitleFile) {
           $("#dropRegion h1").text("Add the subtitle now")
         }
       } else {
-        var videoURL = window.URL.createObjectURL(videoFile);
-        $.get('/videoConversion',{url:videoURL},function(data){
-          console.log("tudo bem camarada");
-        })
-      }
-    } else if (isSubtitle) {
-      subtitleFile = file;
-      track = document.createElement('track');
-      if (format == "text/vtt") {
-        subtitleURL = window.URL.createObjectURL(subtitleFile);
-      } else {
-        var reader = new FileReader();
-        reader.onload = function(e){
-          console.log(reader.result);
-          $.get("/srt2vtt", {
-            subtitle: reader.result,
-            fileName: file.name
-          }, function(vttURL) {
-            track.src = vttURL;
-            console.log("subtitleURL", vttURL);
-          })
+        var r = confirm("Replace the added movie?");
+        if (r == true) {
+          videofile = file;
         }
-        reader.readAsBinaryString(file);
       }
-      if (!videoFile) {
-        $("#dropRegion h1").text("Add the video now")
+
+    } else if (isSubtitle) {
+      if (!subtitleFile) {
+        subtitleFile = file;
+        track = document.createElement('track');
+        if (format == "text/vtt") {
+          subtitleURL = window.URL.createObjectURL(subtitleFile);
+        } else {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            $.get("/srt2vtt", {
+              subtitle: reader.result,
+              fileName: file.name
+            }, function(vttURL) {
+              track.src = vttURL;
+            })
+          }
+          reader.readAsBinaryString(file);
+        }
+        if (!videoFile) {
+          $("#dropRegion h1").text("Add the video now")
+        }
+      } else {
+        var r = confirm("Replace the added subtitle?");
+        if (r == true) {
+          subtitleFile = file;
+        }
       }
     } else {
       alert(format + " isn't supported")
     }
 
     if (videoFile && subtitleFile) {
-
-      console.log(videoFile, subtitleFile);
-
-
-
-
+      //TO FIX
       video.src = window.URL.createObjectURL(videoFile);
-      console.log(video.src);
       track.src = subtitleURL;
 
       $('.container').remove();
@@ -89,12 +87,8 @@ $("#dropRegion").on({
 
       video.controls = true;
       video.play();
-
     }
-
   }
-
-
 })
 
 // blob:http%3A//localhost%3A3000/a379b2cf-8684-4776-86b2-c6fb2391820a blob:http%3A//localhost%3A3000/c3c78ad4-cdf2-4f5d-9c00-7188c13b9dd3
